8ddeac7fae2cedeb13f32bcc80e0e4f6cda584d5
Carnes <whoismaha@gmail.com>
2023-01-29 02:09:51 -0500

    Adds title intro and some free assets

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using Cinemachine;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class TitleScreen : MonoBehaviour
+    {
+        public CinemachineVirtualCamera StartCam;
+        public CinemachineVirtualCamera FlowerCam;
+        public CinemachineVirtualCamera DirtCam;
+        public CinemachineVirtualCamera UndergroundCam;
+
+        public float DelayFromFlowerToDirt = 2;
+        public float DelayFromDirtToUnderground = 2;
+
+        public void Start()
+        {
+            FlowerCam.Priority = 0;
+            DirtCam.Priority = 0;
+            StartCam.Priority = 1000;
+        }
+
+        public void Play()
+        {
+            StartCoroutine(PlayIntroAnimation());
+        }
+
+        private IEnumerator PlayIntroAnimation()
+        {
+            MoveCamera(StartCam, FlowerCam);
+            yield return new WaitForSeconds(DelayFromFlowerToDirt);
+            MoveCamera(FlowerCam, DirtCam);
+            yield return new WaitForSeconds(DelayFromDirtToUnderground);
+            MoveCamera(DirtCam, UndergroundCam);
+        }
+
+        private void MoveCamera(CinemachineVirtualCamera currentCam, CinemachineVirtualCamera newCam)
+        {
+            // Set new camera as highest priority to start the transition
+            newCam.Priority = 1000;
+            currentCam.Priority = 0;
+        }
+
+
+    }
+}
