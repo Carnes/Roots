38a17409feafed324cd3f4e2466bdd72fe5c509d
Teresa Ortiz <tortiz942@gmail.com>
2023-02-04 15:07:08 -0500

    Spawner refactor
    Spawner class can now be used to spawn an objects onto the gameboard

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    public Vector3 minBounds = new Vector3(-25f, -28f);
+    public Vector3 maxBounds = new Vector3(21f, -4f);
+    public float z = 81f;
+    private GameObject[] _existingObjects = Array.Empty<GameObject>();
+
+    void DestroyObjectsIfExists()
+    {
+        if (_existingObjects.Length > 0)
+        {
+            foreach (GameObject existingObjects in _existingObjects)
+            {
+                Destroy(existingObjects);
+            }
+        }
+    }
+    private bool IsTooClose(List<Vector3> objectSpawnPoints, Vector3 randomPosition)
+    {
+        float distanceThreshold = 5f;
+        foreach (Vector3 spawnPoint in objectSpawnPoints)
+        {
+            float distance = Vector3.Distance(randomPosition, spawnPoint);
+            if (distance < distanceThreshold) return true;
+        }
+        return false;
+    }
+    
+    public void RandomlySpawnObjects(GameObject objectToSpawn, int numObjectsToSpawn, string objectTypeToSpawn)
+    {
+        DestroyObjectsIfExists();
+
+        List<Vector3> spawnPoints = new List<Vector3>();
+        for (int i = 0; i < numObjectsToSpawn; i++)
+        {
+            float x = Mathf.Clamp(Random.Range(minBounds.x, maxBounds.x), minBounds.x, maxBounds.x);
+            float y = Mathf.Clamp(Random.Range(minBounds.y, maxBounds.y), minBounds.y, maxBounds.y);
+
+            Vector3 randomPosition = new Vector3(x, y, z);
+
+            if (!IsTooClose(spawnPoints, randomPosition))
+            {
+                spawnPoints.Add(randomPosition);
+
+                var gameObjectToSpawn = Instantiate(objectToSpawn, randomPosition, Quaternion.identity);
+                if (objectTypeToSpawn == "Nutrient")
+                {
+                    var newNutrient = gameObjectToSpawn.GetComponent<Nutrient>();
+                    newNutrient.Value = Random.Range(1,4);
+
+                    float scalingFactor = newNutrient.Value / 2;
+                    gameObjectToSpawn.transform.localScale = new Vector3(scalingFactor, scalingFactor, scalingFactor);
+                }
+            }
+            else i--;
+        }
+        _existingObjects = GameObject.FindGameObjectsWithTag(objectTypeToSpawn);
+    }
+    
+}
