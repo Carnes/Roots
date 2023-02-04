45dc75d0fd480bd6fb9285dc81062e35f272f4d8
kmhaga03 <kmhaga03@gmail.com>
2023-02-03 23:01:47 -0500

    Spider relocates on the y access randomly within the y boundary

--- Assets/Scripts/SpiderController.cs
+++ Assets/Scripts/SpiderController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Roots;
 using UnityEngine;
 using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
@@ -13,6 +14,8 @@ public class SpiderController : MonoBehaviour
     private Vector3 offset;
     private Animator _animator;
     private static readonly int IsWalking = Animator.StringToHash("isWalking");
+    private float startx;
+    private float endx;
 
     public void Awake()
     {
@@ -22,15 +25,29 @@ public class SpiderController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        startx = GameSettings.Instance.BoundaryMin.x;
+        transform.position = new Vector3(startx, transform.position.y, transform.position.z);
         MoveSpider();
+
+      // endx     = GameSettings.Instance.BoundaryMax.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isWalking)
+        if (isWalking) 
         {
-            spider.transform.position += new Vector3(speed*Time.deltaTime, 0, 0); 
+            if (transform.position.x < GameSettings.Instance.BoundaryMax.x)
+            {
+                spider.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            }
+            else
+            {
+                float x = startx;
+                float y = Mathf.Clamp(Random.Range(GameSettings.Instance.BoundaryMin.y, GameSettings.Instance.BoundaryMax.y), GameSettings.Instance.BoundaryMin.y, GameSettings.Instance.BoundaryMax.y);
+                spider.transform.position = new Vector3(x, y, GameSettings.Instance.BoundaryMax.z);
+                
+            }
         }
 
       
