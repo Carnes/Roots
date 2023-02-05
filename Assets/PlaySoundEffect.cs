eb9001e5315e03feeecc365cb31f437581287bce
Teresa Ortiz <tortiz942@gmail.com>
2023-02-05 16:02:17 -0500

    sound effects

---
+++ Assets/PlaySoundEffect.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundEffect : MonoBehaviour
+{
+    public AudioSource seAudioSource;
+    public AudioClip rootHitsRockAudioSound;
+    public AudioClip spiderBreaksRootSound;
+    public AudioClip nutrientPickUpSound;   
+    public AudioClip victorySound;
+    public AudioClip defeatSound;
+    
+    public float volume = 0.5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
