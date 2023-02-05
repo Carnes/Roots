751da6cfdfb9efa8ad65d7e36296657201f3787e
Teresa Ortiz <tortiz942@gmail.com>
2023-02-05 14:48:10 -0500

    victory screen

--- Assets/Scripts/Flower/RootNutrientReserve.cs
+++ Assets/Scripts/Flower/RootNutrientReserve.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -6,18 +8,23 @@ namespace Flower
     public class RootNutrientReserve : Helpers.Singleton<RootNutrientReserve>
     {
         public GameObject deathScreen;
+        public GameObject victoryScreen;
         public float NutrientsInReserve = 10;
         public float DistanceCostMultiplier = 0.5f;
 
         [ContextMenu("Add Nutrients")]
         public void DebugAddNutrient()
         {
-            AddNutrient(100);
+            AddNutrient(50);
         }
 
         public void AddNutrient(float nutrientAmount)
         {
             NutrientsInReserve += nutrientAmount;
+            if (NutrientsInReserve >= 50)
+            {
+                Victory();
+            }
         }
 
         public void SubtractNutrient(float nutrientAmount)
@@ -27,6 +34,11 @@ namespace Flower
             {
                 Death();
             }
+        }
+
+        private void Victory()
+        {
+            victoryScreen.SetActive(true);
         }
 
         private void Death()
